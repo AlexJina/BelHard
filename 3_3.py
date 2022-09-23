67652f44491e7b5d69abae5dec912889ba11e2ad
AlexJina <sas865i@gmail.com>
2022-09-23 18:11:38 +0300

    add spaces

--- 3_3.py
+++ 3_3.py
@@ -11,4 +11,4 @@ print('Моё имя {} , мне {} лет ,я живу в {}'.format(name, age,
 name = input()
 age = int(input())
 city = input()
-print('Моё имя %(name)s , мне %(age)d лет ,я живу в %(city)s' % {"name":name, "age":age, "city":city})
+print('Моё имя %(name)s , мне %(age)d лет ,я живу в %(city)s' % {"name": name, "age": age, "city": city})
