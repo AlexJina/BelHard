d7e2d42808b2383b81b233f60adb5093cd1c98f6
AlexJina <sas865i@gmail.com>
2022-09-23 18:10:23 +0300

    done

--- 3_3.py
+++ 3_3.py
@@ -1,5 +1,14 @@
 name = input()
 age = int(input())
 city = input()
-print(f'Привет, меня зовут {name}, мне {age} лет, я жив в {city}')
+print(f'Привет, меня зовут {name}, мне {age} лет, я живу в {city}')
 
+name = input()
+age = int(input())
+city = input()
+print('Моё имя {} , мне {} лет ,я живу в {}'.format(name, age, city))
+
+name = input()
+age = int(input())
+city = input()
+print('Моё имя %(name)s , мне %(age)d лет ,я живу в %(city)s' % {"name":name, "age":age, "city":city})
