98c42ea351d23f1b256bd01da2ee92826a6420e5
AlexJina <sas865i@gmail.com>
2022-09-28 22:42:40 +0300

    :(

--- 5_1.py
+++ 5_1.py
@@ -3,7 +3,6 @@ m = int(input())
 k = int(input())
 i = 0
 while i < n:
-    if i > k:
-        print(i % m)
+
     i += 1
 print('конец')
---
+++ 5_3.py
@@ -0,0 +1,3 @@
+k = int(input())
+for i in range(2, k):
+    print('\t' + str(i), end='')
