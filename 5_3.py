d44abaeab5e739abe14ad9be468fa34c134bcf8f
AlexJina <sas865i@gmail.com>
2022-09-30 12:23:02 +0300

    done

--- 5_3.py
+++ 5_3.py
@@ -1,10 +1,8 @@
 n = int(input())
-numbers = []
 for i in range(2, n + 1, 10):
-    lst = []
     for j in range(i, i + 9, 2):
         if j > n:
             break
-        lst.append(j)
-    numbers.append(lst.copy())
-print(numbers)
+        print(j, end=' ')
+    print()
+
