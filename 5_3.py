557485814d9ebea577999e0d208b2658d94f0d8e
AlexJina <sas865i@gmail.com>
2022-09-30 12:12:53 +0300

    done

--- 5_3.py
+++ 5_3.py
@@ -1,3 +1,10 @@
-k = int(input())
-for i in range(2, k):
-    print('\t' + str(i), end='')
+n = int(input())
+numbers = []
+for i in range(2, n + 1, 10):
+    lst = []
+    for j in range(i, i + 9, 2):
+        if j > n:
+            break
+        lst.append(j)
+    numbers.append(lst.copy())
+print(numbers)
