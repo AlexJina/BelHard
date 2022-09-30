3db77d1fc39a0c3e3f33f7467e217fc069f4a9f4
AlexJina <sas865i@gmail.com>
2022-09-30 16:44:45 +0300

    + variant from internet

--- 6_3.py
+++ 6_3.py
@@ -1,3 +1,18 @@
 spisok = [1, 2, 3, 4, 5, 6, 7]
 n = int(input())
 print(spisok[-n:] + spisok[:-n])
+
+
+def shik(lst, steps):
+    if steps < 0:
+        steps = abs(steps)
+        for i in range(steps):
+            lst.append(lst.pop(0))
+    else:
+        for i in range(steps):
+            lst.insert(0, lst.pop())
+
+
+spisok = [1, 2, 3, 4, 5, 6, 7]
+shik(spisok, 3)
+print(spisok)
