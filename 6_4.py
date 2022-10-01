3f9ab3179d18e509ceb56fc60e3f834bc038bccd
AlexJina <sas865i@gmail.com>
2022-10-01 08:10:45 +0300

    2 variants

--- 6_4.py
+++ 6_4.py
@@ -1,3 +1,7 @@
 so_list = ['dog', 656, 'cat', 'seryu', 6780, 5, 6, 'kkikl']
 a = [x for x in so_list if type(x) == str]
 print(a)
+
+so_lis = ['dog', 656, 'cat', 'seryu', 6780, 5, 6, 'kkikl']
+so_lis = list(filter(lambda x: type(x) == str, so_lis))
+print(so_lis)
